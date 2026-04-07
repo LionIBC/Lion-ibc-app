5202554e18e0e7c9997d15855b6dd8b7102f867c
LionIBC <mail@bbs-leiste.de>
2026-04-07 22:19:05 +0200

    Update page.js

--- app/mitarbeiter/page.js
+++ app/mitarbeiter/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import FormHeader from '@/components/FormHeader';
+import FormHeader from '../../components/FormHeader';
 
 const initialState = {
   firma: '', vorname: '', nachname: '', geburtsdatum: '', email: '', telefon: '',
