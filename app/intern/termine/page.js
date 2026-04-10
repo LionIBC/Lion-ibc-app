9625475e491c145af476b81f732248afc9ff3067
LionIBC <mail@bbs-leiste.de>
2026-04-10 21:26:10 +0200

    Update page.js

--- app/intern/termine/page.js
+++ app/intern/termine/page.js
@@ -1,1 +1,14 @@
+export default function InternTerminePage() {
+  return (
+    <div>
+      <h1 style={{ fontSize: '28px', fontWeight: '700', color: '#101828' }}>
+        Termine
+      </h1>
+      <p style={{ marginTop: '10px', color: '#667085' }}>
+        Hier erscheinen später interne Termine.
+      </p>
+    </div>
+  );
+}
+
 
