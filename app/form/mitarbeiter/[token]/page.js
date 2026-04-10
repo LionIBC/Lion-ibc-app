e64c0be2cd44ab39d0f2b7f33ffa2d00469f7f36
LionIBC <mail@bbs-leiste.de>
2026-04-10 21:38:26 +0200

    Update page.js

--- app/form/mitarbeiter/[token]/page.js
+++ app/form/mitarbeiter/[token]/page.js
@@ -1,1 +1,20 @@
+export default function MitarbeiterTokenPage({ params }) {
+  const { token } = params;
+
+  return (
+    <div style={{ padding: '40px' }}>
+      <h1 style={{ fontSize: '28px', fontWeight: '700', color: '#101828' }}>
+        Mitarbeiterformular
+      </h1>
+
+      <p style={{ marginTop: '10px', color: '#667085' }}>
+        Dieser Bereich wird aktuell aufgebaut.
+      </p>
+
+      <p style={{ marginTop: '20px', fontSize: '14px', color: '#98a2b3' }}>
+        Token: {token}
+      </p>
+    </div>
+  );
+}
 
