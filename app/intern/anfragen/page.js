4af3d8c50e9d312e885f4ac4a095440ff1d1f0b5
LionIBC <mail@bbs-leiste.de>
2026-04-10 21:22:55 +0200

    Update page.js

--- app/intern/anfragen/page.js
+++ app/intern/anfragen/page.js
@@ -1,1 +1,13 @@
+export default function InternAnfragenPage() {
+  return (
+    <div>
+      <h1 style={{ fontSize: '28px', fontWeight: '700', color: '#101828' }}>
+        Anfragen
+      </h1>
+      <p style={{ marginTop: '10px', color: '#667085' }}>
+        Hier erscheinen später neue Anfragen.
+      </p>
+    </div>
+  );
+}
 
