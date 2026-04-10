b3d326e0f40275e5570b75eac2db48f190159bbf
LionIBC <mail@bbs-leiste.de>
2026-04-10 21:23:58 +0200

    Update page.js

--- app/intern/tickets/page.js
+++ app/intern/tickets/page.js
@@ -1,1 +1,13 @@
+export default function InternTicketsPage() {
+  return (
+    <div>
+      <h1 style={{ fontSize: '28px', fontWeight: '700', color: '#101828' }}>
+        Tickets
+      </h1>
+      <p style={{ marginTop: '10px', color: '#667085' }}>
+        Hier erscheinen später interne Tickets.
+      </p>
+    </div>
+  );
+}
 
