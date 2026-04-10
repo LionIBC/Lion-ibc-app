a802424f58c385c896b20f256dd65d0bef6bcd74
LionIBC <mail@bbs-leiste.de>
2026-04-10 21:23:25 +0200

    Update page.js

--- app/intern/mitarbeitervorgaenge/page.js
+++ app/intern/mitarbeitervorgaenge/page.js
@@ -1,1 +1,13 @@
+export default function InternMitarbeitervorgaengePage() {
+  return (
+    <div>
+      <h1 style={{ fontSize: '28px', fontWeight: '700', color: '#101828' }}>
+        Mitarbeitervorgänge
+      </h1>
+      <p style={{ marginTop: '10px', color: '#667085' }}>
+        Hier erscheinen später Mitarbeitervorgänge.
+      </p>
+    </div>
+  );
+}
 
