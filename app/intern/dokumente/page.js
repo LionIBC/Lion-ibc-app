1a758402018e3abbdfaa2f2dd2301f7e2eb2a851
LionIBC <mail@bbs-leiste.de>
2026-04-10 21:24:31 +0200

    Update page.js

--- app/intern/dokumente/page.js
+++ app/intern/dokumente/page.js
@@ -1,1 +1,13 @@
+export default function InternDokumentePage() {
+  return (
+    <div>
+      <h1 style={{ fontSize: '28px', fontWeight: '700', color: '#101828' }}>
+        Dokumente
+      </h1>
+      <p style={{ marginTop: '10px', color: '#667085' }}>
+        Hier erscheinen später interne Dokumente.
+      </p>
+    </div>
+  );
+}
 
