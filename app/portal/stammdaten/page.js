8b4ecc586224efadfe3e3213253671760ede102c
LionIBC <mail@bbs-leiste.de>
2026-04-10 21:32:13 +0200

    Update page.js

--- app/portal/stammdaten/page.js
+++ app/portal/stammdaten/page.js
@@ -1,1 +1,13 @@
+export default function PortalStammdatenPage() {
+  return (
+    <div>
+      <h1 style={{ fontSize: '28px', fontWeight: '700', color: '#101828' }}>
+        Seite im Aufbau
+      </h1>
+      <p style={{ marginTop: '10px', color: '#667085' }}>
+        Dieser Bereich wird aktuell aufgebaut.
+      </p>
+    </div>
+  );
+}
 
