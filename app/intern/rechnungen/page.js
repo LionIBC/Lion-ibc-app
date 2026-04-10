50173864ea23e0966b93dc0ee510bad1801c57b0
LionIBC <mail@bbs-leiste.de>
2026-04-10 21:25:51 +0200

    Update page.js

--- app/intern/rechnungen/page.js
+++ app/intern/rechnungen/page.js
@@ -1,1 +1,13 @@
+export default function InternRechnungenPage() {
+  return (
+    <div>
+      <h1 style={{ fontSize: '28px', fontWeight: '700', color: '#101828' }}>
+        Rechnungen
+      </h1>
+      <p style={{ marginTop: '10px', color: '#667085' }}>
+        Hier erscheinen später Rechnungen und Zahlungsstatus.
+      </p>
+    </div>
+  );
+}
 
