8dc8226c3ee4f280e692452cd04cc4fa17076f6c
LionIBC <mail@bbs-leiste.de>
2026-04-10 21:25:02 +0200

    Update page.js

--- app/intern/stammdaten/page.js
+++ app/intern/stammdaten/page.js
@@ -1,1 +1,13 @@
+export default function InternStammdatenPage() {
+  return (
+    <div>
+      <h1 style={{ fontSize: '28px', fontWeight: '700', color: '#101828' }}>
+        Stammdaten
+      </h1>
+      <p style={{ marginTop: '10px', color: '#667085' }}>
+        Hier erscheinen später Stammdatenänderungen.
+      </p>
+    </div>
+  );
+}
 
