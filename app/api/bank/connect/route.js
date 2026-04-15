653ea04f8fe5eb09ff9adc86b1d362228d63ac73
LionIBC <mail@bbs-leiste.de>
2026-04-15 14:34:11 +0200

    Update route.js

--- app/api/bank/connect/route.js
+++ app/api/bank/connect/route.js
@@ -1,10 +1,10 @@
+// Datei: app/api/bank/connect/route.js
 export async function GET() {
   const url = `https://auth.truelayer-sandbox.com/?response_type=code
-  &client_id=${process.env.TRUELAYER_CLIENT_ID}
-  &scope=info accounts balance transactions
-  &redirect_uri=${process.env.TRUELAYER_REDIRECT_URI}
-  &providers=uk-ob-all uk-oauth-all es-ob-all
-  `;
+&client_id=${process.env.TRUELAYER_CLIENT_ID}
+&scope=info accounts balance transactions
+&redirect_uri=${process.env.TRUELAYER_REDIRECT_URI}
+&providers=uk-ob-all uk-oauth-all es-ob-all`;
 
   return Response.json({ url });
 }
